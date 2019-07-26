d7419f9c5c63a4e28cf8d8c8b9c3b81c92b36194
scandar <thescandar@gmail.com>
2019-07-26 04:55:08 +0200

    adds testing with mocha and chai

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import debug from 'debug';
 const app = express();
 const log = debug('app');
 
-const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+export const capitalize = (str = '') => str.charAt(0).toUpperCase() + str.slice(1);
 
 app.get('/hello', (req, res) => {
   const { name } = req.query;
@@ -14,3 +14,5 @@ app.get('/hello', (req, res) => {
 app.listen(3000, () => {
   log('server listening on port 3000');
 });
+
+export default app;
--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,46 @@
-describe('hello world', () => {
-  it('returns a message');
-  it('message is Hello World!');
-  it('message has a name from query params');
-  it('name is capitalized');
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app, { capitalize } from '../src/app';
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('/GET hello', () => {
+  let response;
+  beforeEach((done) => {
+    response = undefined;
+
+    chai.request(app)
+      .get('/hello')
+      .end((err, res) => {
+        response = res.body;
+        done();
+      });
+  });
+
+  it('returns a message of type string', () => {
+    response.message.should.be.a('string');
+  });
+  it('message is Hello World!', () => {
+    response.message.should.equal('Hello World!');
+  });
+});
+
+describe('/GET hello with query params', () => {
+  const name = 'foo';
+  let response;
+  beforeEach((done) => {
+    response = undefined;
+
+    chai.request(app)
+      .get(`/hello?name=${name}`)
+      .end((err, res) => {
+        response = res.body;
+        done();
+      });
+  });
+
+  it('message has a capitalized name from query params', () => {
+    response.message.should.equal(`Hello ${capitalize(name)}!`);
+  });
 });
